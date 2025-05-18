d97927bfaf0cbd798b88d4bc52c8cc969b185528
Mark Murnane <mark@hackafe.net>
2025-05-18 15:56:51 -0400

    Adding EBS storage

--- platform/addons.py
+++ platform/addons.py
@@ -7,3 +7,34 @@ import nodegroup
 eks_pod_identity_agent = aws.eks.Addon("eks-pod-identity-agent",
     cluster_name=eks.eks_cluster.name,
     addon_name="eks-pod-identity-agent")
+
+ebs_role = aws.iam.Role("ebs-csi",
+    name = "ebs-csi",
+    assume_role_policy=json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "AllowEksAuthToAssumeRoleForPodIdentity",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "pods.eks.amazonaws.com"
+                },
+                "Action": [
+                    "sts:AssumeRole",
+                    "sts:TagSession"
+                ]
+            }
+        ]
+    }))
+
+aws.iam.RolePolicyAttachment("ebs_policy",
+    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
+    role=ebs_role.name)
+
+ebs_csi_driver = aws.eks.Addon("aws-ebs-csi-driver",
+    cluster_name=eks.eks_cluster.name,
+    addon_name="aws-ebs-csi-driver",
+    pod_identity_associations=[{
+        "role_arn": ebs_role.arn,
+        "service_account": "ebs-csi-controller-sa"
+    }])
