e174a3aa332c3ee78bb9adeb7fba3035893b53b9
Mark Murnane <mark@hackafe.net>
2026-01-29 22:26:07 -0500

    Expanding subnets

--- platform/vpc.py
+++ platform/vpc.py
@@ -28,8 +28,10 @@ if not config.get("skip_internet_gateway"):
     # Create a route to the internet gateway
     aws.ec2.Route("public-internet-route", route_table_id=public_route_table.id, destination_cidr_block="0.0.0.0/0", gateway_id=internet_gateway.id)
 
-cidr = ipaddress.IPv4Network(config.require("subnet_cidr_block"))
+cidr = ipaddress.IPv4Network(config.require("cidr_block"))
 subnets = cidr.subnets(prefixlen_diff=config.require_int("subnet_prefixlen"))
+for i in range(config.require_int("subnet_cidr_block")):
+    next(subnets)
 private_subnets = []
 public_subnets = []
 if config.get_object("private_subnet_ids"):
