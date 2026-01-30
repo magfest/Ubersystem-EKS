2f1d734d1e5dff67537a2d79f7d27b26c897eff5
Mark Murnane <mark@hackafe.net>
2026-01-29 21:47:33 -0500

    Adding name transform

--- platform/__main__.py
+++ platform/__main__.py
@@ -7,3 +7,22 @@ import cert
 import cnpg
 import ubersystem
 import cloudfront
+
+import pulumi
+def alias_old_project_name(args):
+    """
+    Automatically adds an alias to every resource to tell Pulumi 
+    that it used to belong to the 'magfest-uber' project.
+    """
+    # Create an alias that points to the SAME resource name, 
+    # but in the OLD project name.
+    return pulumi.ResourceTransformationResult(
+        args.props,
+        pulumi.ResourceOptions(
+            aliases=[pulumi.Alias(project="magfest-uber")], 
+            parent=args.opts.parent if args.opts else None
+        )
+    )
+
+# Register the transformation globally for this run
+pulumi.runtime.register_stack_transformation(alias_old_project_name)
