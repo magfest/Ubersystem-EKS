aa69c765384bfeb818098beb0359c8a642ff9657
Mark Murnane <mark@hackafe.net>
2026-07-26 18:37:25 -0400

    Allow disabling cloudfront

--- platform/cert.py
+++ platform/cert.py
@@ -3,51 +3,54 @@ import pulumi_aws as aws
 
 config = pulumi.Config()    
 
-main_domain = config.require("wildcard_domain")
-san_list = config.get_object("subject_alternative_names") or []
+disable_cloudfront = config.get_bool("disable_cloudfront", False)
 
-certificate_arn = config.get("certificate_arn")
-if not certificate_arn:
-    certificate = aws.acm.Certificate("ubersystem",
-        domain_name=main_domain,
-        subject_alternative_names=san_list,
-        validation_method="DNS"
-    )
+if not disable_cloudfront:
+    main_domain = config.require("wildcard_domain")
+    san_list = config.get_object("subject_alternative_names") or []
 
-    validation_records = []
-
-    all_domains = list(set([main_domain] + san_list))
-    all_domains.sort()
-
-    for i, domain in enumerate(all_domains):
-        zone = aws.route53.get_zone(
-            name=domain.replace("*.", ""), 
-            private_zone=False
+    certificate_arn = config.get("certificate_arn")
+    if not certificate_arn:
+        certificate = aws.acm.Certificate("ubersystem",
+            domain_name=main_domain,
+            subject_alternative_names=san_list,
+            validation_method="DNS"
         )
-        
-        def get_domain_validation_option(args):
-            dvos, domain_name = args
-            for dvo in dvos:
-                if dvo['domain_name'] == domain_name:
-                    return dvo
-            return dvos[0]
 
-        validation_option = pulumi.Output.all(certificate.domain_validation_options, domain).apply(get_domain_validation_option)
+        validation_records = []
 
-        record = aws.route53.Record(f"validation-record-{i}",
-            allow_overwrite=True,
-            name=validation_option['resource_record_name'],
-            records=[validation_option['resource_record_value']],
-            type=validation_option['resource_record_type'],
-            zone_id=zone.id,
-            ttl=60
+        all_domains = list(set([main_domain] + san_list))
+        all_domains.sort()
+
+        for i, domain in enumerate(all_domains):
+            zone = aws.route53.get_zone(
+                name=domain.replace("*.", ""), 
+                private_zone=False
+            )
+            
+            def get_domain_validation_option(args):
+                dvos, domain_name = args
+                for dvo in dvos:
+                    if dvo['domain_name'] == domain_name:
+                        return dvo
+                return dvos[0]
+
+            validation_option = pulumi.Output.all(certificate.domain_validation_options, domain).apply(get_domain_validation_option)
+
+            record = aws.route53.Record(f"validation-record-{i}",
+                allow_overwrite=True,
+                name=validation_option['resource_record_name'],
+                records=[validation_option['resource_record_value']],
+                type=validation_option['resource_record_type'],
+                zone_id=zone.id,
+                ttl=60
+            )
+            
+            validation_records.append(record.fqdn)
+
+        aws.acm.CertificateValidation("cert-validation",
+            certificate_arn=certificate.arn,
+            validation_record_fqdns=validation_records
         )
-        
-        validation_records.append(record.fqdn)
 
-    aws.acm.CertificateValidation("cert-validation",
-        certificate_arn=certificate.arn,
-        validation_record_fqdns=validation_records
-    )
-
-    certificate_arn = certificate.arn
+        certificate_arn = certificate.arn
--- platform/cloudfront.py
+++ platform/cloudfront.py
@@ -6,6 +6,8 @@ import vpc
 import cert
 config = pulumi.Config()
 
+disable_cloudfront = config.get_bool("disable_cloudfront", False)
+
 nlb_sg = aws.ec2.SecurityGroup("nginx-nlb-sg",
     vpc_id=vpc.vpc_id,
     description="Security group for internal ALB",
@@ -67,76 +69,77 @@ nginx_listener = aws.lb.Listener("nginx-listener",
     }]
 )
 
-vpc_origin = aws.cloudfront.VpcOrigin("vpc-origin",
-    vpc_origin_endpoint_config={
-        "name": "uber",
-        "arn": nginx_nlb.arn,
-        "http_port": 80,
-        "https_port": 443,
-        "origin_protocol_policy": "http-only",
-        "origin_ssl_protocols": {
-            "items": ["TLSv1.2"],
-            "quantity": 1,
-        },
-    }
-)
+if not disable_cloudfront:
+    vpc_origin = aws.cloudfront.VpcOrigin("vpc-origin",
+        vpc_origin_endpoint_config={
+            "name": "uber",
+            "arn": nginx_nlb.arn,
+            "http_port": 80,
+            "https_port": 443,
+            "origin_protocol_policy": "http-only",
+            "origin_ssl_protocols": {
+                "items": ["TLSv1.2"],
+                "quantity": 1,
+            },
+        }
+    )
 
-s3_distribution = aws.cloudfront.Distribution("s3_distribution",
-    origins=[{
-        "domain_name": nginx_nlb.dns_name,
-        "origin_id": config.require("cluster_name"),
-        "vpc_origin_config": {
-            "vpc_origin_id": vpc_origin.id, 
-        }
-    }],
-    enabled=True,
-    is_ipv6_enabled=True,
-    default_cache_behavior={
-        "allowed_methods": ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
-        "cached_methods": ["GET", "HEAD", "OPTIONS"],
-        "target_origin_id": config.require("cluster_name"),
-        "viewer_protocol_policy": "redirect-to-https",
-        "min_ttl": 0,
-        "default_ttl": 900,
-        "max_ttl": 3600,
-        "forwarded_values": {
-            "query_string": True,
-            "headers": ["*"],
-            "cookies": {
-                "forward": "all"
+    s3_distribution = aws.cloudfront.Distribution("s3_distribution",
+        origins=[{
+            "domain_name": nginx_nlb.dns_name,
+            "origin_id": config.require("cluster_name"),
+            "vpc_origin_config": {
+                "vpc_origin_id": vpc_origin.id, 
             }
-        }
-    },
-    ordered_cache_behaviors=[
-        {
-            "path_pattern": "/static*",
-            "allowed_methods": ["GET", "HEAD", "OPTIONS"],
+        }],
+        enabled=True,
+        is_ipv6_enabled=True,
+        default_cache_behavior={
+            "allowed_methods": ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
             "cached_methods": ["GET", "HEAD", "OPTIONS"],
             "target_origin_id": config.require("cluster_name"),
+            "viewer_protocol_policy": "redirect-to-https",
             "min_ttl": 0,
             "default_ttl": 900,
             "max_ttl": 3600,
-            "compress": True,
-            "viewer_protocol_policy": "redirect-to-https",
             "forwarded_values": {
-                "query_string": False,
-                "headers": ["Host"],
+                "query_string": True,
+                "headers": ["*"],
                 "cookies": {
-                    "forward": "none"
+                    "forward": "all"
                 }
             }
-        }
-    ],
-    price_class="PriceClass_100",
-    viewer_certificate={
-        "acm_certificate_arn": cert.certificate_arn,
-        "ssl_support_method": "sni-only",
-    },
-    restrictions={
-        "geo_restriction": {
-            "restriction_type": "none",
-            "locations": []
-        }
-    },
-    aliases=[config.require("wildcard_domain"), *(config.get_object("subject_alternative_names") or [])]
-)
+        },
+        ordered_cache_behaviors=[
+            {
+                "path_pattern": "/static*",
+                "allowed_methods": ["GET", "HEAD", "OPTIONS"],
+                "cached_methods": ["GET", "HEAD", "OPTIONS"],
+                "target_origin_id": config.require("cluster_name"),
+                "min_ttl": 0,
+                "default_ttl": 900,
+                "max_ttl": 3600,
+                "compress": True,
+                "viewer_protocol_policy": "redirect-to-https",
+                "forwarded_values": {
+                    "query_string": False,
+                    "headers": ["Host"],
+                    "cookies": {
+                        "forward": "none"
+                    }
+                }
+            }
+        ],
+        price_class="PriceClass_100",
+        viewer_certificate={
+            "acm_certificate_arn": cert.certificate_arn,
+            "ssl_support_method": "sni-only",
+        },
+        restrictions={
+            "geo_restriction": {
+                "restriction_type": "none",
+                "locations": []
+            }
+        },
+        aliases=[config.require("wildcard_domain"), *(config.get_object("subject_alternative_names") or [])]
+    )
